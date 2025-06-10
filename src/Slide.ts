30a15111ad1ac36a1d8be4ac0721451145df62ba
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-06-10 16:06:18 +0200

    alteracao de escopo

--- src/Slide.ts
+++ src/Slide.ts
@@ -85,6 +85,7 @@ export default class Slide {
     this.show(next);
   }
   pause() {
+    document.body.classList.add("paused");
     this.pausedTimeout = new Timeout(() => {
       this.timeout?.pause();
       this.paused = true;
@@ -95,6 +96,7 @@ export default class Slide {
     }, 300);
   }
   continue() {
+    document.body.classList.remove("paused");
     this.pausedTimeout?.clear();
     if (this.paused) {
       this.paused = false;
@@ -115,7 +117,8 @@ export default class Slide {
     prevButton.addEventListener("pointerup", () => this.prev());
     nextButton.addEventListener("pointerup", () => this.next());
     this.controls.addEventListener("pointerdown", () => this.pause());
-    this.controls.addEventListener("pointerup", () => this.continue());
+    document.addEventListener("pointerup", () => this.continue());
+    document.addEventListener("touchend", () => this.continue());
   }
   private addThumbItems() {
     const thumbContainer = document.createElement("div");
